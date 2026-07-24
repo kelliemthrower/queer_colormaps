7be01ef1b4355e0f52bb6deca7589d7a5e035662
kelliemthrower <kelliemthrower@outlook.com>
2026-07-24 10:15:58 -0500

    Update lesbian_flag.py

--- lesbian_flag.py
+++ lesbian_flag.py
@@ -6,10 +6,10 @@ import numpy as np
 colors = ["#D62E02", "#FD9855", "#FFFFFF", "#D161A2", '#A20160'] 
 
 # 2. Create the colormap object
-custom_cmap = LinearSegmentedColormap.from_list("my_custom_map", colors, N=24)
+lesbian = LinearSegmentedColormap.from_list("my_custom_map", colors, N=24)
 
 # 3. Test it with sample data
 data = np.linspace(-3, 3, 100).reshape(10, 10)
-plt.imshow(data, cmap=custom_cmap)
+plt.imshow(data, cmap=lesbian)
 plt.colorbar()
 plt.show()
