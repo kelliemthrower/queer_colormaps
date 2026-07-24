510d394511c1cf9907c4be3f47c26492a79bf113
kelliemthrower <kelliemthrower@outlook.com>
2026-07-24 10:17:13 -0500

    Update gay_flag.py

--- gay_flag.py
+++ gay_flag.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 import numpy as np
 
-#colormap for gay flag
+# colormap for gay flag
 colors = ["#078D70", "#26CEAA", "#98E8C1", "#7BADE2", '#5049CC', '#3D1A78'] 
 
-# 2. Create the colormap object
+# create the colormap object, N=number of colors inside the gradient, customize as needed!
 gay = LinearSegmentedColormap.from_list("my_custom_map", colors, N=24)
 
-# 3. Test it with sample data
+# test it with sample data, (don't import this into your code, just for testing)
 data = np.linspace(-3, 3, 100).reshape(10, 10)
 plt.imshow(data, cmap=gay)
 plt.colorbar()
